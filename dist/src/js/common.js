37a8ecd795467c292f77e1fdfaace0f6c1391a90
CrocodileDaniil <karasev_dan_an@mail.ru>
2024-12-19 19:13:58 +0300

    выполнены компании

--- dist/src/js/common.js
+++ dist/src/js/common.js
@@ -1,15 +1,20 @@
-$(function() {
+$(function () {
+  new WOW().init();
 
-	new WOW().init();
+  $(".hamburger").click(function () {
+    $(this).toggleClass("is-active");
 
-	$(".hamburger").click(function(){
-		$(this).toggleClass("is-active");
+    if ($(this).hasClass("is-active")) {
+      $(".mnu_top").slideDown(300);
+    } else {
+      $(".mnu_top").slideUp(300);
+    }
+  });
 
-		if($(this).hasClass('is-active')){
-			$('.mnu_top').slideDown(300);
-		}else{
-			$('.mnu_top').slideUp(300);
-		}
-	});
+  /* Reviews tabs */
 
+  $(".companies > img").click(function () {
+    const revId = $(this).data("revid");
+    console.log("revId");
+  });
 });
--- src/js/common.js
+++ src/js/common.js
@@ -1,15 +1,26 @@
-$(function() {
+$(function () {
+  new WOW().init();
 
-	new WOW().init();
+  $(".hamburger").click(function () {
+    $(this).toggleClass("is-active");
 
-	$(".hamburger").click(function(){
-		$(this).toggleClass("is-active");
+    if ($(this).hasClass("is-active")) {
+      $(".mnu_top").slideDown(300);
+    } else {
+      $(".mnu_top").slideUp(300);
+    }
+  });
 
-		if($(this).hasClass('is-active')){
-			$('.mnu_top').slideDown(300);
-		}else{
-			$('.mnu_top').slideUp(300);
-		}
-	});
+  /* Reviews tabs */
 
+  $(".companies > img").click(function() {
+    const revId = $(this).data("revid");
+		$(".companies > img").not(this).removeClass('active')
+		$(this).addClass('active')
+
+		$(".rew_inner > .item_rev").not(revId).removeClass('active')
+		$(revId).addClass('active')
+
+    console.log(revId);
+  });
 });
