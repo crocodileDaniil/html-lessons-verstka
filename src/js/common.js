bcecbe43ffaea18c224e48ca3b2999ae473538b8
CrocodileDaniil <karasev_dan_an@mail.ru>
2025-02-02 15:00:09 +0300

    end mobile adaptive

--- src/js/common.js
+++ src/js/common.js
@@ -1,16 +1,19 @@
 $(function () {
   new WOW().init();
 
+  /*mobile open menu */
   $(".hamburger").click(function () {
     $(this).toggleClass("is-active");
 
     if ($(this).hasClass("is-active")) {
       $(".mnu_top").slideDown(300);
+      $(".mnu_top").addClass("mnu_top-flex");
     } else {
       $(".mnu_top").slideUp(300);
+      $(".mnu_top").removeClass("mnu_top-flex");
     }
   });
-
+  /*mobile open menu */
   /* Reviews tabs */
 
   $(".companies > img").click(function() {
